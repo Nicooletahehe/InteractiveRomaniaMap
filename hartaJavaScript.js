5d7c36115342050625f4e2e1c6836611751bbc31
nicooletahehe <nicoleta7u@yahoo.com>
2021-01-14 16:37:43 +0200

    legenda

--- hartaJavaScript.js
+++ hartaJavaScript.js
@@ -1,22 +1,7 @@
-// console.log(document.querySelectorAll(".judete"));
 let judete = document.querySelectorAll("path");
 let tabel = document.querySelector("#tabelJudet");
 var slider = document.getElementById("rangeAni");
 var output = document.getElementById("spanAn");
-
-// console.log(document.querySelectorAll("path"));
-//buton schimbare culoare judete random
-// function schimbCuloare() {
-//     for(let i=0; i<judete.length;i++){
-//         const r = Math.floor(Math.random() * 256);
-//         const g = Math.floor(Math.random() * 256);
-//         const b = Math.floor(Math.random() * 256);
-//         judete[i].style.fill = 'rgb('+r+','+g+","+b+')';
-//     };
-// }
-
-// let buton = document.querySelector("#butonSchimbCuloare");
-// buton.addEventListener('click', function(){schimbCuloare()});
 
 let request;
 let dataJudet = [];
@@ -26,7 +11,6 @@ var jsonData = [];
 
 //parsam toate jsoanele
 let loadFile = function (filePath, done) {
-    // var xhr = new XMLHTTPRequest();
     request = new XMLHttpRequest();
     request.onload = function () { return done(this.responseText) }
     request.open("GET", filePath, true);
@@ -42,57 +26,12 @@ myFiles.forEach(function (file, i) {
 console.log(jsonData);
 console.log(jsonData[1]);
 
-
-output.innerHTML = slider.value;
-slider.oninput = function() {
-    output.innerHTML = this.value;
-}
-
-let valoareSlider = document.querySelectorAll("#rangeAni")[0].value;
-function loadTable(e){
-    for(let z=0;z<jsonData[0].length;z++){
-        // console.log(slider.value);
-        // console.log(jsonData[0][z].an);
-        //imi aduca datele de pe anul selectat in slider
-        if(jsonData[0][z].an === parseInt(slider.value)) {
-            console.log("vedefg");
-            //verific numele judetului selectat, si incarc doar acele date
-            let numeJudetSVG = judete[e].attributes[2].value;
-            for(let j=0;j<jsonData[0][z].date.length;j++){
-                console.log(jsonData[0][z].date[j].nume);
-                if(jsonData[0][z].date[j].nume === numeJudetSVG){
-                    console.log(jsonData[0][z].date[j].nume);
-                    let numeJudetTabel = document.querySelector("#titluJudet");
-                    let numeMunicipiuTabel = document.querySelector("#numeMunicipiu");
-                    let nrPopulatieTabel = document.querySelector("#nrPopulatie");
-                    let suprafataTabel = document.querySelector("#suprafata");
-                    let densitateTabel = document.querySelector("#densitate");
-                    let somajTabel = document.querySelector("#somaj");
-                    let anTabel = document.querySelector("#anTabel");
-                    console.log(jsonData[0][z].an);
-                    numeJudetTabel.innerHTML = jsonData[0][z].date[j].nume;
-                    numeMunicipiuTabel.innerHTML = jsonData[0][z].date[j].municipiu;
-                    nrPopulatieTabel.innerHTML = jsonData[0][z].date[j].populatie;
-                    suprafataTabel.innerHTML = jsonData[0][z].date[j].suprafata;
-                    densitateTabel.innerHTML = jsonData[0][z].date[j].densitate;
-                    somajTabel.innerHTML = jsonData[0][z].date[j].somaj;
-                    anTabel.innerHTML = jsonData[0][z].an;
-                }
-            }
-        }
-    }
-}
-
 //slider
 output.innerHTML = slider.value;
 slider.oninput = function () {
     //depastrat
     var procentualArray = []
     output.innerHTML = this.value;
-
-   // console.log(this.value)
-
-
     for (let i = 0; i < judete.length; i++) {
 
         //console.log(jsonData[0]);
@@ -106,8 +45,10 @@ slider.oninput = function () {
                 
                 if (this.value <= 1992) {
                     judete[i].style.fill = 'rgb(' + 124 + ',' + 124 + ',' + 124 + ')';
+                    document.querySelector("#noData").style.display = 'block';
                 }
                 else {
+                    document.querySelector("#noData").style.display = 'none';
                     let procentData = jsonData[2][j][this.value - 1992]
                     //console.log(procentData)
                     if (procentData < -1) {
@@ -142,6 +83,45 @@ slider.oninput = function () {
             }
         }
 
+    }
+}
+
+let valoareSlider = document.querySelectorAll("#rangeAni")[0].value;
+function loadTable(e){
+    for(let z=0;z<jsonData[0].length;z++){
+        // console.log(slider.value);
+        // console.log(jsonData[0][z].an);
+        //imi aduca datele de pe anul selectat in slider
+        if(jsonData[0][z].an === parseInt(slider.value)) {
+            console.log("vedefg");
+            //verific numele judetului selectat, si incarc doar acele date
+            let numeJudetSVG = judete[e].attributes[2].value;
+            for(let j=0;j<jsonData[0][z].date.length;j++){
+                // console.log(jsonData[0][z].date[j].nume);
+                if(jsonData[0][z].date[j].nume === numeJudetSVG){
+                    console.log(jsonData[0][z].date[j].nume);
+                    let numeJudetTabel = document.querySelector("#titluJudet");
+                    let numeMunicipiuTabel = document.querySelector("#numeMunicipiu");
+                    let nrPopulatieTabel = document.querySelector("#nrPopulatie");
+                    let suprafataTabel = document.querySelector("#suprafata");
+                    let densitateTabel = document.querySelector("#densitate");
+                    let somajTabel = document.querySelector("#somaj");
+                    let anTabel = document.querySelector("#anTabel");
+                    // console.log(jsonData[0][z].an);
+                    let jsonPopSeparator = jsonData[0][z].date[j].populatie.toLocaleString('en-US').replace(',', ' ');
+                    let jsonSupSeparator = jsonData[0][z].date[j].suprafata.toLocaleString('en-US').replace(',', ' ');
+                    let jsonSomSeparator = jsonData[0][z].date[j].somaj.toLocaleString('en-US').replace(',', ' ');
+                    numeJudetTabel.innerHTML = jsonData[0][z].date[j].nume;
+                    numeMunicipiuTabel.innerHTML = jsonData[0][z].date[j].municipiu;
+                    nrPopulatieTabel.innerHTML = jsonPopSeparator;
+                    suprafataTabel.innerHTML = jsonSupSeparator + ' km2';
+                    let jsonSuprafataZecimal = parseFloat(jsonData[0][z].date[j].densitate.replace(/,/g, '.')).toFixed(2);
+                    densitateTabel.innerHTML = jsonSuprafataZecimal + '/km2';
+                    somajTabel.innerHTML = jsonSomSeparator;
+                    anTabel.innerHTML = jsonData[0][z].an;
+                }
+            }
+        }
     }
 }
 
