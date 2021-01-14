e8a7ccd9288c9ff27014f09cbbc2a4f2b64aedf5
nicooletahehe <nicoleta7u@yahoo.com>
2021-01-14 12:39:48 +0200

    remodificat

--- hartaJavaScript.js
+++ hartaJavaScript.js
@@ -6,22 +6,22 @@ var output = document.getElementById("spanAn");
 
 // console.log(document.querySelectorAll("path"));
 //buton schimbare culoare judete random
-function schimbCuloare() {
-    for(let i=0; i<judete.length;i++){
-        const r = Math.floor(Math.random() * 256);
-        const g = Math.floor(Math.random() * 256);
-        const b = Math.floor(Math.random() * 256);
-        judete[i].style.fill = 'rgb('+r+','+g+","+b+')';
-    };
-}
+// function schimbCuloare() {
+//     for(let i=0; i<judete.length;i++){
+//         const r = Math.floor(Math.random() * 256);
+//         const g = Math.floor(Math.random() * 256);
+//         const b = Math.floor(Math.random() * 256);
+//         judete[i].style.fill = 'rgb('+r+','+g+","+b+')';
+//     };
+// }
 
-let buton = document.querySelector("#butonSchimbCuloare");
-buton.addEventListener('click', function(){schimbCuloare()});
+// let buton = document.querySelector("#butonSchimbCuloare");
+// buton.addEventListener('click', function(){schimbCuloare()});
 
 let request;
 let dataJudet = [];
 let dataPopulatie;
-let myFiles = [ "dateJudete.json", "datePopulatie.json" ];
+let myFiles = [ "dateJudete.json", "datePopulatie.json", "dateProcentuale.json" ];
 var jsonData = [];
 
 //parsam toate jsoanele
@@ -80,6 +80,68 @@ function loadTable(e){
                 }
             }
         }
+    }
+}
+
+//slider
+output.innerHTML = slider.value;
+slider.oninput = function () {
+    //depastrat
+    var procentualArray = []
+    output.innerHTML = this.value;
+
+   // console.log(this.value)
+
+
+    for (let i = 0; i < judete.length; i++) {
+
+        //console.log(jsonData[0]);
+        let numeJudetSVG = judete[i].attributes[2].value;
+        //for (let x = 0; x < judete.length; x++) { judete[x].style.fill = ''; }
+        for (let j = 0; j < jsonData[2].length; j++) {
+            //console.log(jsonData[0][i]);
+            // console.log(numeJudetSVG)
+            // console.log(jsonData[2][j][0])
+            if (jsonData[2][j][0] === numeJudetSVG) {
+                
+                if (this.value <= 1992) {
+                    judete[i].style.fill = 'rgb(' + 124 + ',' + 124 + ',' + 124 + ')';
+                }
+                else {
+                    let procentData = jsonData[2][j][this.value - 1992]
+                    //console.log(procentData)
+                    if (procentData < -1) {
+                        judete[i].style.fill = "#0C1C41";
+                    }
+                    else if (procentData < -0.3) {
+                        judete[i].style.fill = "#20325E";
+                    }
+                    else if (procentData < - 0.2) {
+                        judete[i].style.fill = "#364975";
+                    }
+                    else if (procentData < -0.1) {
+                        judete[i].style.fill = "#566890";
+                    }
+                    else if (procentData >= -0.1 && procentData <= 0.1) {
+                        judete[i].style.fill = "#AFAF45";
+                    }
+                    else if (procentData > 1) {
+                        judete[i].style.fill = "#610808";
+                    }
+                    else if (procentData > 0.3) {
+                        judete[i].style.fill = "#8C2424";
+                    }
+                    else if (procentData > 0.2) {
+                        judete[i].style.fill = "#AF4545";
+                    }
+                    else if (procentData > 0.1) {
+                        judete[i].style.fill = "#D77777";
+                    }
+                }
+                break;
+            }
+        }
+
     }
 }
 
